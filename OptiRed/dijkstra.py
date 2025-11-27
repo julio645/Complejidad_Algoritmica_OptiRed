76b93761227f62058ea6d7c22ade04ab6bcbc84e
julio645 <julio030807@gmail.com>
2025-11-26 20:36:20 -0500

    Delete OptiRed directory

--- OptiRed/dijkstra.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx
-
-def ejecutar_dijkstra(G, origen, destino):
-    try:
-        path = nx.dijkstra_path(G, origen, destino, weight='weight')
-        distancia = nx.dijkstra_path_length(G, origen, destino, weight='weight')
-        return path, distancia
-    except:
-        return None, None
