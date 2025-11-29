372561659dd47103b46a8d68f3128edff2e0ae4a
Eslander-Celis <eslander_celis@hotmail.com>
2025-11-29 01:45:17 -0500

    modificar_dijkstra

--- OptiRed/dijkstra.py
+++ OptiRed/dijkstra.py
@@ -1,9 +1,58 @@
-import networkx as nx
+import math
+import heapq as hq
+
+def dijkstra(G, start):
+  # Diccionarios para manejar nodos con nombre
+  visited = {n: False for n in G.nodes()}
+  cost = {n: math.inf for n in G.nodes()}
+  parent = {n: None for n in G.nodes()}
+
+  cost[start] = 0
+  pq = [(0, start)]
+
+
+  while pq:
+
+    g, u = hq.heappop(pq)
+
+    if not visited[u]:
+      visited[u] = True
+    
+      for v in G[u]:
+
+        w = G[u][v]['weight'] # peso de la arista u—v
+        f = g + w
+
+
+
+        if not visited[v] and f < cost[v]:
+          cost[v] = f
+          parent[v] = u
+          hq.heappush(pq, (f, v))
+
+  return parent, cost
+
+
+
+
 
 def ejecutar_dijkstra(G, origen, destino):
-    try:
-        path = nx.dijkstra_path(G, origen, destino, weight='weight')
-        distancia = nx.dijkstra_path_length(G, origen, destino, weight='weight')
-        return path, distancia
-    except:
-        return None, None
+
+  parent, cost = dijkstra(G, origen)
+
+  if cost[destino] == math.inf:
+    return None, None
+  
+  # reconstruir ruta
+  camino = []
+  actual = destino
+
+  while actual is not None:
+    camino.append(actual)
+    actual = parent[actual]
+
+
+  camino.reverse()
+
+  return camino, cost[destino]
+
